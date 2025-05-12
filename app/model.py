a195102bf4ba25286eafbd34d7ec5af3a44a598a
Jasiek <u119_jangaw_waw@technischools.com>
2025-05-12 18:50:36 +0200

    model.py

---
+++ app/model.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field, field_validator
+from datetime import datetime
+
+
+class AirQualityDataValidation(BaseModel):
+    timestamp: datetime
+    pm10: float = Field(..., ge=0, le=1000)
+    pm2_5: float = Field(..., ge=0, le=1000)
+    co: float = Field(..., ge=0, le=50)
+    no2: float = Field(..., ge=0)
+    so2: float = Field(..., ge=0)
+    ozone: float = Field(..., ge=0)
+
+    @field_validator("timestamp", mode="before")
+    @classmethod
+    def validate_timestamp(cls, value):
+        if isinstance(value, str):
+            try:
+                return datetime.fromisoformat(value)
+            except ValueError:
+                raise ValueError("Invalid datetime format. Use ISO format.")
+        return value
