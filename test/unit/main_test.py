b9584773ee86418a0e8bbd71582286858d6cd542
Jasiek <u119_jangaw_waw@technischools.com>
2025-05-12 19:29:13 +0200

    maintest & model test

--- test/unit/main_test.py
+++ test/unit/main_test.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import pytest
 from main import setup
 
@@ -13,6 +14,26 @@ def client():
 def test_api_data_missing_datetime(client):
     response = client.get("/api/data")
     assert response.status_code == 400
-    assert response.is_json
-    data = response.get_json()
-    assert "Missing 'datetime'" in data.get("An error occurred", "")
+
+
+def test_api_data_post_success(client):
+    payload = {
+        "timestamp": datetime.utcnow().isoformat(),
+        "pm10": 12.5,
+        "pm2_5": 7.3,
+        "co": 0.8,
+        "no2": 15.0,
+        "so2": 5.0,
+        "ozone": 30.2
+    }
+    response = client.post("/api/data", json=payload)
+    assert response.status_code == 201
+
+
+def test_api_data_get_no_data(client):
+    app = setup()
+    app.config["TESTING"] = True
+    with app.test_client() as test_client:
+        dt = datetime.utcnow().isoformat()
+        response = test_client.get(f"/api/data?datetime={dt}")
+        assert response.status_code == 404
---
+++ test/unit/model_test.py
@@ -0,0 +1,108 @@
+import pytest
+from app.model import AirQualityDataValidation
+from datetime import datetime
+
+def test_valid_reading():
+    data = {
+        "timestamp": "2024-05-01T12:00:00",
+        "pm10": 20,
+        "pm2_5": 10,
+        "co": 0.5,
+        "no2": 15,
+        "so2": 3,
+        "ozone": 25,
+    }
+    reading = AirQualityDataValidation.model_validate(data)
+    assert reading.pm10 == 20
+    assert isinstance(reading.timestamp, datetime)
+
+def test_invalid_pm10():
+    data = {
+        "timestamp": "2024-05-01T12:00:00",
+        "pm10": -5,
+        "pm2_5": 10,
+        "co": 0.5,
+        "no2": 15,
+        "so2": 3,
+        "ozone": 25,
+    }
+    with pytest.raises(Exception):
+        AirQualityDataValidation.model_validate(data)
+
+def test_invalid_pm2_5():
+    data = {
+        "timestamp": "2024-05-01T12:00:00",
+        "pm10": 10,
+        "pm2_5": 100000,
+        "co": 0.5,
+        "no2": 15,
+        "so2": 3,
+        "ozone": 25,
+    }
+    with pytest.raises(Exception):
+        AirQualityDataValidation.model_validate(data)
+
+def test_invalid_co():
+    data = {
+        "timestamp": "2024-05-01T12:00:00",
+        "pm10": 5,
+        "pm2_5": 10,
+        "co": -10,
+        "no2": 15,
+        "so2": 3,
+        "ozone": 25,
+    }
+    with pytest.raises(Exception):
+        AirQualityDataValidation.model_validate(data)
+
+def test_invalid_no2():
+    data = {
+        "timestamp": "2024-05-01T12:00:00",
+        "pm10": 5,
+        "pm2_5": 10,
+        "co": 0.5,
+        "no2": -10,
+        "so2": 3,
+        "ozone": 25,
+    }
+    with pytest.raises(Exception):
+        AirQualityDataValidation.model_validate(data)
+
+def test_invalid_so2():
+    data = {
+        "timestamp": "2024-05-01T12:00:00",
+        "pm10": 5,
+        "pm2_5": 10,
+        "co": 0.5,
+        "no2": 15,
+        "so2": -10,
+        "ozone": 25,
+    }
+    with pytest.raises(Exception):
+        AirQualityDataValidation.model_validate(data)
+
+def test_invalid_ozone():
+    data = {
+        "timestamp": "2024-05-01T12:00:00",
+        "pm10": -5,
+        "pm2_5": 10,
+        "co": 0.5,
+        "no2": 15,
+        "so2": 3,
+        "ozone": -2137,
+    }
+    with pytest.raises(Exception):
+        AirQualityDataValidation.model_validate(data)
+
+def test_invalid_timestamp():
+    data = {
+        "timestamp": "TechniSchools",
+        "pm10": -5,
+        "pm2_5": 10,
+        "co": 0.5,
+        "no2": 15,
+        "so2": 3,
+        "ozone": -2137,
+    }
+    with pytest.raises(Exception):
+        AirQualityDataValidation.model_validate(data)
